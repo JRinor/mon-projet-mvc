0648b0e09517071188e2af518aafaecb97e18e59
Januzi Rinor <rinor.januzi@outlook.com>
2024-11-15 06:49:51 +0100

    Initial commit for mon-projet-mvc

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
+const port = 5000;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+app.get('/', async (req, res) => {
+  const { rows } = await pool.query('SELECT NOW()');
+  res.send(rows[0]);
+});
+
+app.listen(port, () => {
+  console.log(`Backend running on port ${port}`);
+});
