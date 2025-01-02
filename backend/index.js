710a6f548617c7dcfbd41baa17d23f888603af03
JRinor <rinor.januzi7@etu.univ-lorraine.fr>
2025-01-02 02:08:24 +0100

    Correction port

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
-const port = 5000;
+const port = 5001;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
